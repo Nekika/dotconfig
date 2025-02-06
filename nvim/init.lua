b63173fa92ce58e93ccd7443d23f323a87dbe197
Nekika <hugo.jahnke@gmail.com>
2025-02-06 14:05:23 +0100

    neovim: updated plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,5 @@
+vim.g.mapleader = " "
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
@@ -10,82 +12,7 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
+
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  {
-    "rebelot/kanagawa.nvim",
-    opts = {
-      background = {
-        dark = "wave",
-        light = "lotus"
-      }
-    },
-    init = function()
-      vim.cmd.colorscheme "kanagawa"
-    end
-  },
-  -- {
-  --   name = "tulipe",
-  --   dir = "~/Projects/tulipe/tulipe.nvim",
-  --   dev = true,
-  --   lazy = false,
-  --   opts = {},
-  -- },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    dependencies = {
-      { "ms-jpq/coq_nvim",      branch = "coq" },
-      { "ms-jpq/coq.artifacts", branch = "artifacts" }
-    },
-    init = function()
-      vim.g.coq_settings = {
-        auto_start = "shut-up"
-      }
-    end
-  },
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = {
-      'nvim-tree/nvim-web-devicons'
-    },
-    opts = {
-      options = {
-        theme = "auto"
-      }
-    }
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    main = "nvim-treesitter.configs",
-    opts = {
-      sync_install = true,
-      auto_install = true,
-      highlight = {
-        enable = true
-      }
-    }
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = {
-      "nvim-lua/plenary.nvim"
-    },
-    opts = {}
-  },
-  {
-    "ThePrimeagen/refactoring.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    opts = {},
-  },
-  {
-    "tpope/vim-fugitive",
-  }
-})
-
-require("nekika")
+require("lazy").setup({ import = "nekika/plugins" })
