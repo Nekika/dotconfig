dfee3376bc8127f80857d0e7875ed454ea646957
Nekika <hugo.jahnke@gmail.com>
2025-05-12 10:24:30 +0200

    neovim: added stylua as lua formatter

--- nvim/lua/nekika/plugins/conform.lua
+++ nvim/lua/nekika/plugins/conform.lua
@@ -1,16 +1,17 @@
 return {
-  "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      css = { "prettier" },
-      go = { "gofmt" },
-      html = { "prettier" },
-      javascript = { "prettier" },
-      json = { "prettier" },
-      typescript = { "prettier" },
-      vue = { "prettier" },
-      yaml = { "prettier" },
-      zig = { "zigfmt" },
-    }
-  }
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			css = { "prettier" },
+			go = { "gofmt" },
+			html = { "prettier" },
+			javascript = { "prettier" },
+			json = { "prettier" },
+			lua = { "stylua" },
+			typescript = { "prettier" },
+			vue = { "prettier" },
+			yaml = { "prettier" },
+			zig = { "zigfmt" },
+		},
+	},
 }
