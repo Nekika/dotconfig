28c097d0bb125ffd6cd4da3a8b0cbb275701d3c3
Nekika <hugo.jahnke@gmail.com>
2025-05-28 16:49:36 +0200

    neovim: migrated lsp setup to native vim.lsp functions

--- nvim/lua/nekika/plugins/conform.lua
+++ nvim/lua/nekika/plugins/conform.lua
@@ -1,17 +1,28 @@
 return {
-	"stevearc/conform.nvim",
-	opts = {
-		formatters_by_ft = {
-			css = { "prettier" },
-			go = { "gofmt" },
-			html = { "prettier" },
-			javascript = { "prettier" },
-			json = { "prettier" },
-			lua = { "stylua" },
-			typescript = { "prettier" },
-			vue = { "prettier" },
-			yaml = { "prettier" },
-			zig = { "zigfmt" },
-		},
-	},
+    "stevearc/conform.nvim",
+    opts = {
+        formatters_by_ft = {
+            css = { "prettier" },
+            go = { "gofmt" },
+            html = { "prettier" },
+            javascript = { "prettier" },
+            json = { "prettier" },
+            lua = { "stylua" },
+            typescript = { "prettier" },
+            vue = { "prettier" },
+            yaml = { "prettier" },
+            zig = { "zigfmt" },
+        },
+    },
+    config = function(_, opts)
+        local conform = require("conform")
+
+        conform.setup(opts)
+
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            callback = function(args)
+                conform.format({ bufnr = args.buf })
+            end,
+        })
+    end,
 }
--- nvim/lua/nekika/plugins/lsp.lua
+++ nvim/lua/nekika/plugins/lsp.lua
@@ -1,93 +1,98 @@
 return {
-  "neovim/nvim-lspconfig",
-  lazy = false,
-  dependencies = {
-    "saghen/blink.cmp",
-  },
-  config = function()
-    local capabilities = require("blink.cmp").get_lsp_capabilities()
-    local lspconfig = require("lspconfig")
-
-    local configs = {
-      { name = "cssls" },
-      { name = "dockerls" },
-      {
-        name = "elixirls",
-        options = {
-          cmd = { "/usr/local/share/elixir-ls/language_server.sh" }
-        }
-      },
-      { name = "gopls" },
-      { name = "html" },
-      { name = "jsonls" },
-      {
-        name = "lua_ls",
-        options = {
-          settings = {
-            Lua = {
-              runtime = {
-                version = "LuaJIT",
-              },
-              diagnostics = {
-                globals = { "require", "vim" },
-              },
-              workspace = {
-                library = vim.api.nvim_get_runtime_file("", true),
-              },
-              telemetry = {
-                enable = false,
-              },
-            }
-          }
-        }
-      }, {
-      name = "ts_ls",
-      options = {
-        init_options = {
-          plugins = {
+    "neovim/nvim-lspconfig",
+    config = function()
+        local configs = {
+            { "cssls" },
+            { "dockerls" },
             {
-              name = "@vue/typescript-plugin",
-              location = os.getenv("NODE_GLOBAL_MODULES_DIR") .. "/@vue/language-server",
-              languages = { "vue" },
-            }
-          }
-        },
-        filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "vue" },
-      }
-    },
-      { name = "volar" },
-      { name = "zls" },
-    }
+                "elixirls",
+                {
+                    cmd = { "/usr/local/share/elixir-ls/language_server.sh" },
+                },
+            },
+            { "gopls" },
+            { "html" },
+            { "jsonls" },
+            {
+                "lua_ls",
+                {
+                    settings = {
+                        Lua = {
+                            runtime = {
+                                version = "LuaJIT",
+                            },
+                            diagnostics = {
+                                globals = { "require", "vim" },
+                            },
+                            workspace = {
+                                library = vim.api.nvim_get_runtime_file(
+                                    "",
+                                    true
+                                ),
+                            },
+                            telemetry = {
+                                enable = false,
+                            },
+                        },
+                    },
+                },
+            },
+            {
+                "ts_ls",
+                {
+                    init_options = {
+                        plugins = {
+                            {
+                                "@vue/typescript-plugin",
+                                location = os.getenv("NODE_GLOBAL_MODULES_DIR")
+                                    .. "/@vue/language-server",
+                                languages = { "vue" },
+                            },
+                        },
+                    },
+                    filetypes = {
+                        "javascript",
+                        "javascriptreact",
+                        "javascript.jsx",
+                        "typescript",
+                        "typescriptreact",
+                        "typescript.tsx",
+                        "vue",
+                    },
+                },
+            },
+            { "vue_ls" },
+            { "zls" },
+        }
 
-    for _, config in ipairs(configs) do
-      config.options = config.options or {}
-      config.options.capabilities = capabilities
-      lspconfig[config.name].setup(config.options)
-    end
+        for _, config in ipairs(configs) do
+            vim.lsp.config(config[1], config[2] or {})
+            vim.lsp.enable(config[1])
+        end
 
-    vim.api.nvim_create_autocmd("LspAttach", {
-      callback = function(args)
-        vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
+        vim.api.nvim_create_autocmd("LspAttach", {
+            callback = function()
+                vim.keymap.set(
+                    "n",
+                    "<C-[>",
+                    vim.lsp.buf.type_definition,
+                    { buffer = 0, desc = "Go to type definition" }
+                )
 
-        vim.keymap.set("i", "<C-CR>", "<C-x><C-o>", { buffer = 0, desc = "Trigger completion" })
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0, desc = "Display documentation" })
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0, desc = "Go to the definition" })
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0, desc = "Go to declaration" })
-        vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0, desc = "Go to type definition" })
-        vim.keymap.set("n", "<F6>", vim.lsp.buf.rename, { buffer = 0, desc = "Rename all references" })
-        vim.keymap.set({ "i", "n" }, "<F2>", vim.lsp.buf.code_action, { buffer = 0, desc = "List code actions" })
-      end
-    })
+                vim.keymap.set(
+                    { "i", "n" },
+                    "<F2>",
+                    vim.lsp.buf.code_action,
+                    { buffer = 0, desc = "List code actions" }
+                )
 
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      callback = function(args)
-        require("conform").format({ bufnr = args.buf })
-        -- vim.lsp.buf.format({
-        --   bufnr = args.buf,
-        --   async = false,
-        --   timeout_ms = 10000,
-        -- })
-      end,
-    })
-  end,
+                vim.keymap.set(
+                    "n",
+                    "<F6>",
+                    vim.lsp.buf.rename,
+                    { buffer = 0, desc = "Rename all references" }
+                )
+            end,
+        })
+    end,
 }
